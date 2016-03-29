fca8a3755c2bd1fb0509ee0fa8407e34e6047079
kaimou <kaimou1357>
2016-03-29 16:02:40 -0400

    Relative path for FB framework.

--- RateMyFashion/LoginController.m
+++ RateMyFashion/LoginController.m
@@ -20,6 +20,7 @@
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
     loginButton.center = self.view.center;
     [self.view addSubview:loginButton];
+    //[self fetchUserInfo];
     
     // Do any additional setup after loading the view, typically from a nib.
     
@@ -29,9 +30,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 - (IBAction)buttonPressed:(id)sender {
-    NSLog(@"Button Pressed!");
+    [self fetchUserInfo];
+}
+
+-(void)fetchUserInfo
+{
+    if ([FBSDKAccessToken currentAccessToken])
+    {
+        NSLog(@"Token is available : %@",[[FBSDKAccessToken currentAccessToken]tokenString]);
+        
+        [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:@{@"fields": @"id, name, link, first_name, last_name, picture.type(large), email, birthday, bio ,location ,friends ,hometown , friendlists"}]
+         startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+             if (!error)
+             {
+                 NSLog(@"resultis:%@",result);
+             }
+             else
+             {
+                 NSLog(@"Error %@",error);
+             }
+         }];
+        
+    }
+    
 }
 
 @end
